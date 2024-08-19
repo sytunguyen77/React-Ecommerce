e2d8c38def642a0420e076905c52089f044af679
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2024-08-19 04:07:50 -0500

    add galleries

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,47 +2,57 @@ import React from "react";
 import Helmet from "../components/Helmet";
 
 const Contact = () => {
-   const handleSubmit = (event) => {
-      event.preventDefault();
-   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-   return (
-      <Helmet title="Contact">
-         <div className="contact content">
-            <h2 className="contact__heading">Feel Free To Contact Us</h2>
-            <div className="contact__form form">
-               <form onSubmit={handleSubmit}>
-                  <div className="input-form">
-                     <span>Username</span>
-                     <input type="text" name="username" placeholder="Username" required />
-                  </div>
-                  <div className="input-form">
-                     <span>Email</span>
-                     <input type="email" name="email" placeholder="Email" required />
-                  </div>
-                  <div className="input-form">
-                     <span>Enter your message</span>
-                     <textarea name="message" placeholder="Your message" required />
-                  </div>
-                  <div className="input-form">
-                     <input type="submit" value="Send" />
-                  </div>
-               </form>
+  return (
+    <Helmet title="Contact">
+      <div className="contact content">
+        <h2 className="contact__heading">Feel Free To Contact Us</h2>
+        <div className="contact__form form">
+          <form onSubmit={handleSubmit}>
+            <div className="input-form">
+              <span>Full Name</span>
+              <input
+                type="text"
+                name="fullName"
+                placeholder="Enter your full name"
+                required
+              />
             </div>
-            <div className="contact__map">
-               <iframe
-                  title="Google Map Location"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.4972874270593!2d-122.30609482278051!3d47.6553350711944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549014929d8535eb%3A0x6b742c7901b82ba3!2sUniversity%20of%20Washington!5e0!3m2!1sen!2sus!4v1685079638731!5m2!1sen!2sus"
-                  width="100%"
-                  height="450"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  referrerPolicy="no-referrer-when-downgrade"
-               ></iframe>
+            <div className="input-form">
+              <span>Email</span>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+              />
             </div>
-         </div>
-      </Helmet>
-   );
+            <div className="input-form">
+              <span>Enter your message</span>
+              <textarea name="message" placeholder="Your message" required />
+            </div>
+            <div className="input-form">
+              <input type="submit" value="Send" />
+            </div>
+          </form>
+        </div>
+        <div className="contact__map">
+          <iframe
+            title="Google Map Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.4972874270593!2d-122.30609482278051!3d47.6553350711944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549014929d8535eb%3A0x6b742c7901b82ba3!2sUniversity%20of%20Washington!5e0!3m2!1sen!2sus!4v1685079638731!5m2!1sen!2sus"
+            width="100%"
+            height="350"
+            style={{ border: 0 }}
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </Helmet>
+  );
 };
 
 export default Contact;
--- src/pages/Galleries.jsx
+++ src/pages/Galleries.jsx
@@ -1,12 +1,101 @@
 import React from "react";
-import coming from "../assets/images/coming-soon.jpg";
 import Helmet from "../components/Helmet";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "swiper/css/autoplay";
+
+import heroImage1 from "../assets/images/galleries/gallery-01.jpg";
+import heroImage2 from "../assets/images/galleries/gallery-02.jpg";
+import heroImage3 from "../assets/images/galleries/gallery-03.jpg";
+import image1 from "../assets/images/galleries/gallery-04.jpg";
+import image2 from "../assets/images/galleries/gallery-05.jpg";
+import image3 from "../assets/images/galleries/gallery-06.jpg";
+import image4 from "../assets/images/galleries/gallery-07.jpg";
 
 const Galleries = () => {
   return (
     <Helmet title="Galleries">
       <div className="galleries">
-        <img className="galleries__img" src={coming} alt="" />
+        <div className="galleries__hero">
+          <Swiper
+            modules={[Navigation, Pagination, EffectFade, Autoplay]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            loop={true}
+            effect="fade"
+            fadeEffect={{ crossFade: true }}
+            speed={500}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+          >
+            <SwiperSlide>
+              <img
+                src={heroImage1}
+                alt="Hero 1"
+                className="galleries__hero-img"
+              />
+              <div className="galleries__hero-text">[ you only live once ]</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src={heroImage2}
+                alt="Hero 2"
+                className="galleries__hero-img"
+              />
+              <div className="galleries__hero-text">[ don't hold back ]</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src={heroImage3}
+                alt="Hero 3"
+                className="galleries__hero-img"
+              />
+              <div className="galleries__hero-text">[ seize the day ]</div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="galleries__content">
+          <div className="galleries__grid">
+            <div className="galleries__grid-item">
+              <img
+                src={image1}
+                alt="Gallery 1"
+                className="galleries__grid-img"
+              />
+            </div>
+            <div className="galleries__grid-item">
+              <img
+                src={image2}
+                alt="Gallery 2"
+                className="galleries__grid-img"
+              />
+            </div>
+            <div className="galleries__grid-item">
+              <img
+                src={image3}
+                alt="Gallery 3"
+                className="galleries__grid-img"
+              />
+            </div>
+            <div className="galleries__grid-item">
+              <img
+                src={image4}
+                alt="Gallery 4"
+                className="galleries__grid-img"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </Helmet>
   );
